00d4840ad172f67f65aecb036471c3a028a9ec0a
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-06-07 17:19:22 +0000

    changes in 3 files

---
+++ js/splashScene.js
@@ -0,0 +1,40 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Kenny Le and Marshall Demars All rights reserved
+//
+// Created by: Kenny Le and Marshall Demars
+// Created on: June 2022
+// This is the Splash Scene
+
+/**
+ * This class is the Splash Scene.
+ */
+class SplashScene extends Phaser.Scene {
+    /**
+   * This method is the construtor.
+   */
+  constructor() {
+    super({ key: "splashScene" })
+  }
+
+  /**
+   * This method is the init.
+   */
+  init(data) {
+    this.cameras.main.setBackgroundColor("ffffff")
+  }
+
+ /**
+   * This method is the preload.
+   */
+  preload() {
+    console.log('Splash Scene')
+
+  create (data) {
+    // pass
+  }
+
+  update(time, delta) {
+    // pass
+  }
+}
