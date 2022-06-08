45a16fd144ccc199194e310d159d7f7e7cc83618
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-06-08 17:24:02 +0000

    menuScene js, game.js, gameScene.js

---
+++ js/gameScene.js
@@ -0,0 +1,49 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Kenny Le and Marshall Demars All rights reserved
+//
+// Created by: Kenny Le and Marshall Demars
+// Created on: June 2022
+// This is the game Scene
+
+/**
+ * This class is the Game Scene.
+ */
+class GameScene extends Phaser.Scene {
+  /**
+   * This method is the construtor.
+   */
+  constructor() {
+    super({ key: "gameScene" })
+  }
+
+  /**
+   * This method is the init.
+   */
+  init(data) {
+    this.cameras.main.setBackgroundColor("ffffff")
+  }
+
+  /**
+   * This method is the preload.
+   */
+  preload() {
+    console.log("Game Scene")
+  }
+
+  /**
+   * This method is the create.
+   */
+  create(data) {
+    // pass
+  }
+
+  /**
+   * This method is the update.
+   */
+  update(time, delta) {
+    // pass
+  }
+}
+
+export default GameScene
---
+++ js/menuScene.js
@@ -0,0 +1,64 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Kenny Le and Marshall Demars All rights reserved
+//
+// Created by: Kenny Le and Marshall Demars
+// Created on: June 2022
+// This is the Menu Scene
+
+/**
+ * This class is the Menu Scene.
+ */
+class MenuScene extends Phaser.Scene {
+  /**
+   * This method is the construtor.
+   */
+  constructor() {
+    super({ key: "menuScene" })
+
+    this.menuSceneBackgroundImage = null
+    this.startButton = null
+  }
+
+  /**
+   * This method is the init.
+   */
+  init(data) {
+    this.cameras.main.setBackgroundColor("ffffff")
+  }
+
+  /**
+   * This method is the preload.
+   */
+  preload() {
+    console.log("Menu Scene")
+    this.load.image('menuSceneBackground', 'assets/fruits_screen_image2.jpg')
+    this.load.image('startButton', 'assets/start.png')
+  }
+
+  /**
+   * This method is the create.
+   */
+  create(data) {
+    this.menuSceneBackgroundImage = this.add.sprite(0, 0, 'menuSceneBackground')
+    this.menuSceneBackgroundImage.x = 1920 / 2
+    this.menuSceneBackgroundImage.y = 1080 / 2
+
+    this.startButton = thisButton = this.add.sprite(1920 / 2, (1080 / 2) + 100, 'startButton')
+    this.startButton.setInteractive({ useHandCursor: true })
+    this.startButton.on('pointerdown', () => this.clickButton())
+  }
+
+  /**
+   * This method is the update.
+   */
+  update(time, delta) {
+    //pass
+  }
+
+  clickButton () {
+    this.scene.start('gameScene')
+  }
+}
+
+export default MenuScene
