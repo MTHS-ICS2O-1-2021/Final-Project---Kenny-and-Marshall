2c66bcf877082cefc022250632b4dee474a5dbf9
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-06-10 17:25:32 +0000

    changes to gameScene.js

--- js/gameScene.js
+++ js/gameScene.js
@@ -34,14 +34,18 @@ class GameScene extends Phaser.Scene {
 
     // images
     this.load.image("skyBackground", "assets/sky-background.png")
+    this.load.image('basket', 'assets/basket.png')
+    this.load.image('apple', 'assets/apple.png')
   }
 
   /**
    * This method is the create.
    */
   create(data) {
-    this.background = this.add.image(0, 0, "skyBackground").setScale(3.5)
+    this.background = this.add.image(0, 0, "skyBackground").setScale(3.7)
     this.background.setOrigin(0, 0)
+
+    this.basket = this.physics.add.sprite(1920 / 2, 1080 - 100, 'basket').setScale(0.5)
     // pass
   }
 
@@ -49,6 +53,24 @@ class GameScene extends Phaser.Scene {
    * This method is the update.
    */
   update(time, delta) {
+    // 60 times a second
+    
+    const keyLeftObj = this.input.keyboard.addKey('LEFT')
+    const keyRightObj = this.input.keyboard.addKey('RIGHT')
+
+    if (keyLeftObj.isDown === true) {
+      this.basket.x -= 25
+      if (this.basket.x < 0) {
+        this.basket.x = 1920
+      }
+    }
+
+    if (keyRightObj.isDown === true) {
+      this.basket.x += 25
+      if (this.basket.x > 1920) {
+        this.basket.x = 0
+      }
+    }
     // pass
   }
 }
