9dd297b8d36b07ce7fd28e08eca720a3effdc77c
Kenny-Le-281 <Kenny-Le-281@users.noreply.github.com>
2022-06-13 17:27:15 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -10,51 +10,58 @@
  * This class is the Game Scene.
  */
 class GameScene extends Phaser.Scene {
-  
   // create an apple
-  createApple () {
+  createApple() {
     const appleXLocation = Math.floor(Math.random() * 1920) + 1
     let appleXVelocity = Math.floor(Math.random() * 50) + 1
     appleXVelocity *= Math.round(Math.random()) ? 1 : -1
-    const anApple = this.physics.add.sprite(appleXLocation, 100, 'apple').setScale(0.4)
+    const anApple = this.physics.add
+      .sprite(appleXLocation, 100, "apple")
+      .setScale(0.4)
     anApple.body.velocity.y = 200
     anApple.body.velocity.x = appleXVelocity
-    this.appleGroup.add(anApple)  
+    this.appleGroup.add(anApple)
   }
 
   // create a banana
-  createBanana () {
+  createBanana() {
     const bananaXLocation = Math.floor(Math.random() * 1920) + 1
     let bananaXVelocity = Math.floor(Math.random() * 50) + 1
     bananaXVelocity *= Math.round(Math.random()) ? 1 : -1
-    const anBanana = this.physics.add.sprite(bananaXLocation, 100, 'banana').setScale(0.3)
+    const anBanana = this.physics.add
+      .sprite(bananaXLocation, 100, "banana")
+      .setScale(0.3)
     anBanana.body.velocity.y = 200
     anBanana.body.velocity.x = bananaXVelocity
-    this.bananaGroup.add(anBanana)  
+    this.bananaGroup.add(anBanana)
   }
 
   // create a pineapple
-  createPineapple () {
+  createPineapple() {
     const pineappleXLocation = Math.floor(Math.random() * 1920) + 1
     let pineappleXVelocity = Math.floor(Math.random() * 50) + 1
     pineappleXVelocity *= Math.round(Math.random()) ? 1 : -1
-    const anPineapple = this.physics.add.sprite(pineappleXLocation, 100, 'pineapple').setScale(0.3)
+    const anPineapple = this.physics.add
+      .sprite(pineappleXLocation, 100, "pineapple")
+      .setScale(0.3)
     anPineapple.body.velocity.y = 700
     anPineapple.body.velocity.x = pineappleXVelocity
-    this.pineappleGroup.add(anPineapple)  
+    this.pineappleGroup.add(anPineapple)
   }
 
   // create a spike
-  createSpike () {
+  createSpike() {
     const spikeXLocation = Math.floor(Math.random() * 1920) + 1
     let spikeXVelocity = Math.floor(Math.random() * 50) + 1
     spikeXVelocity *= Math.round(Math.random()) ? 1 : -1
-    const anSpike = this.physics.add.sprite(spikeXLocation, 100, 'spike').setScale(0.3)
+    const anSpike = this.physics.add
+      .sprite(spikeXLocation, 100, "spike")
+      .setScale(0.3)
     anSpike.body.velocity.y = 200
     anSpike.body.velocity.x = spikeXVelocity
-    this.spikeGroup.add(anSpike)  
+    this.spikeGroup.add(anSpike)
   }
-  
+
   /**
    * This method is the construtor.
    */
@@ -114,29 +121,45 @@ class GameScene extends Phaser.Scene {
     this.createSpike()
 
     // Collisions between basket and apple
-    this.physics.add.collider(this.basket, this.appleGroup, function (basketCollide, appleCollide) {
-      appleCollide.destroy()
-      this.createApple()
-    }.bind(this))
+    this.physics.add.collider(
+      this.basket,
+      this.appleGroup,
+      function (basketCollide, appleCollide) {
+        appleCollide.destroy()
+        this.createApple()
+      }.bind(this)
+    )
 
     // Collisions between basket and banana
-    this.physics.add.collider(this.basket, this.bananaGroup, function (basketCollide, bananaCollide) {
-      bananaCollide.destroy()
-      this.createBanana()
-    }.bind(this))
+    this.physics.add.collider(
+      this.basket,
+      this.bananaGroup,
+      function (basketCollide, bananaCollide) {
+        bananaCollide.destroy()
+        this.createBanana()
+      }.bind(this)
+    )
 
     // Collisions between basket and pineapple
-    this.physics.add.collider(this.basket, this.pineappleGroup, function (basketCollide, pineappleCollide) {
-      pineappleCollide.destroy()
-      this.createPineapple()
-    }.bind(this))
+    this.physics.add.collider(
+      this.basket,
+      this.pineappleGroup,
+      function (basketCollide, pineappleCollide) {
+        pineappleCollide.destroy()
+        this.createPineapple()
+      }.bind(this)
+    )
 
     // Collisions between basket and spike
-    this.physics.add.collider(this.basket, this.spikeGroup, function (basketCollide, spikeCollide) {
-      spikeCollide.destroy()
-      this.createSpike()
-    }.bind(this))
-    
+    this.physics.add.collider(
+      this.basket,
+      this.spikeGroup,
+      function (basketCollide, spikeCollide) {
+        spikeCollide.destroy()
+        this.createSpike()
+      }.bind(this)
+    )
+
     // pass
   }
 
